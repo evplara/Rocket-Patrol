e538f2b31c12b9cf3a5ad1494a398bc6fe91d5f2
Evan Lara <evplara@ucsc.edu>
2025-01-28 17:36:39 -0800

    Final maybe

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 //Evan Lara
 //Rocket Patrol Deluxe Edition 
 //15 hours
-//Implemented Background Music (1 Point), Control for Rocket After it is Fired (1 Point),
+//Implemented Background Music (1 Point), Toggle for Control for Rocket After it is Fired (1 Point),
 //  A on screen timer (3 Points), 
 // Time Mechanic that adds and subtracts time for hits and misses (5 Points,) Mouse Control Movement Option (5 points), and Particle Explosion when Ships are hit (5 Points)
 //I \ used Phaser Documentation and Phaser Example Projects https://phaser.io/examples/v3.85.0 as well as  GitHub User rexrainbow Phaser Notes which I found through a Google Search https://rexrainbow.github.io/phaser3-rex-notes/docs/site/ 
